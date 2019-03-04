c10927939a557ef9fb9d2fbbd89082be93018299
Adrian Shum <adrian.shum@goxip.com>
2019-03-04 14:46:09 +0800

    env.Env

--- env/env.go
+++ env/env.go
@@ -7,24 +7,24 @@ import (
 	"go.uber.org/zap"
 )
 
-// Map env loader based on envy, and show only loaded values
-type Map struct {
+// Env env loader based on envy, and show only loaded values
+type Env struct {
 	// only loaded on startup, should no need bother concurrent
 	m map[string]string
 	l *sync.RWMutex
 }
 
 // New env loader
-func New(files ...string) *Map {
-	envy.Load(files...) // load .env file
-	return &Map{
+func New(files ...string) *Env {
+	_ = envy.Load(files...) // load .env file
+	return &Env{
 		m: make(map[string]string),
 		l: &sync.RWMutex{},
 	}
 }
 
 // Get env loader
-func (m *Map) Get(key string, value string) string {
+func (m *Env) Get(key string, value string) string {
 	m.l.Lock()
 	defer m.l.Unlock()
 	val := envy.Get(key, value)
@@ -32,15 +32,15 @@ func (m *Map) Get(key string, value string) string {
 	return val
 }
 
-// Map env loader, show only loaded values
-func (m *Map) Map() map[string]string {
+// Env env loader, show only loaded values
+func (m *Env) Map() map[string]string {
 	m.l.RLock()
 	defer m.l.RUnlock()
 	return m.m
 }
 
 // ZapFields return env map in for of zap.Field slices
-func (m *Map) ZapFields() []zap.Field {
+func (m *Env) ZapFields() []zap.Field {
 	m.l.RLock()
 	defer m.l.RUnlock()
 	fields := make([]zap.Field, 0, len(m.m))
