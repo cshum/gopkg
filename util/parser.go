b34668fe115f043512bda5270fd6eb8f89775557
Adrian Shum <adrian.shum@goxip.com>
2019-03-12 10:27:41 +0800

    cleanup

--- util/parser.go
+++ util/parser.go
@@ -21,8 +21,8 @@ func init() {
 	validate = validator.New()
 }
 
-// QueryParse decode req query and validate struct from string map
-func QueryParse(r *http.Request, dst interface{}) error {
+// ParseQuery decode req query and validate struct from string map
+func ParseQuery(r *http.Request, dst interface{}) error {
 	err := decoder.Decode(dst, r.URL.Query())
 	if err != nil {
 		return err
@@ -34,8 +34,8 @@ func QueryParse(r *http.Request, dst interface{}) error {
 	return nil
 }
 
-// BodyParse decode req body and validate struct from string map
-func BodyParse(r *http.Request, dst interface{}) error {
+// ParseBody decode req body and validate struct from string map
+func ParseBody(r *http.Request, dst interface{}) error {
 	if isBodyJSON(r) {
 		reader := io.LimitReader(r.Body, int64(10<<20)) // 10MB
 		body, err := ioutil.ReadAll(reader)
