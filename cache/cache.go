e50cdc204d9a0920007a2b8f02a8a2f58de7937e
Adrian Shum <adrian@cshum.com>
2019-07-07 20:44:15 +0800

    cache maxLocalTTL

--- cache/cache.go
+++ cache/cache.go
@@ -8,31 +8,33 @@ import (
 )
 
 type Cache struct {
-	Redis *redis.Client
-	Local *cache.Cache
+	Redis       *redis.Client
+	Local       *cache.Cache
+	maxLocalTTL time.Duration
 }
 
-func New(client *redis.Client, defaultTTL, cleanupInterval time.Duration) *Cache {
+func New(client *redis.Client, maxLocalTTL, cleanupInterval time.Duration) *Cache {
 	return &Cache{
-		Redis: client,
-		Local: cache.New(defaultTTL, cleanupInterval),
+		Redis:       client,
+		Local:       cache.New(maxLocalTTL, cleanupInterval),
+		maxLocalTTL: maxLocalTTL,
 	}
 }
 
 func (c *Cache) Get(key string) ([]byte, error) {
-	// check local first
 	if res, ok := c.Local.Get(key); ok {
 		return res.([]byte), nil
 	}
-	// then check redis
 	res, err := c.Redis.Get(key).Result()
 	return []byte(res), err
 }
 
 func (c *Cache) Set(key string, value []byte, ttl time.Duration) error {
-	// then set local
-	c.Local.Set(key, value, ttl)
-	// set redis first
+	if ttl > c.maxLocalTTL {
+		c.Local.Set(key, value, c.maxLocalTTL)
+	} else {
+		c.Local.Set(key, value, ttl)
+	}
 	if _, err := c.Redis.Set(key, value, ttl).Result(); err != nil {
 		return err
 	}
