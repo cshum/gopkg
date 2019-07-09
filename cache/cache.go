3067f861e0c537f88ef2b411dadc6d653ec4065a
Adrian Shum <adrian.shum@goxip.com>
2019-07-09 10:27:11 +0800

    local cache tweaks

--- cache/cache.go
+++ cache/cache.go
@@ -13,11 +13,10 @@ type Cache struct {
 	maxLocalTTL time.Duration
 }
 
-func New(client *redis.Client, maxLocalTTL, cleanupInterval time.Duration) *Cache {
+func New(client *redis.Client, cleanupInterval time.Duration) *Cache {
 	return &Cache{
-		Redis:       client,
-		Local:       cache.New(maxLocalTTL, cleanupInterval),
-		maxLocalTTL: maxLocalTTL,
+		Redis: client,
+		Local: cache.New(time.Minute*30, cleanupInterval),
 	}
 }
 
@@ -30,13 +29,7 @@ func (c *Cache) Get(key string) ([]byte, error) {
 }
 
 func (c *Cache) Set(key string, value []byte, ttl time.Duration) error {
-	if ttl > c.maxLocalTTL {
-		c.Local.Set(key, value, c.maxLocalTTL)
-	} else {
-		c.Local.Set(key, value, ttl)
-	}
-	if _, err := c.Redis.Set(key, value, ttl).Result(); err != nil {
-		return err
-	}
-	return nil
+	c.Local.Set(key, value, ttl)
+	_, err := c.Redis.Set(key, value, ttl).Result()
+	return err
 }
