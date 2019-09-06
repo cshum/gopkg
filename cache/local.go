9c2fc935a78fc1fbe6d5c2121c98f3836bb397b5
Adrian Shum <adrian.shum@goxip.com>
2019-09-06 11:12:10 +0800

    Local cache

---
+++ cache/local.go
@@ -0,0 +1,29 @@
+package cache
+
+import (
+	"time"
+
+	"github.com/patrickmn/go-cache"
+)
+
+type Local struct {
+	Cache *cache.Cache
+}
+
+func NewLocal(defaultTTL, cleanupInterval time.Duration) *Local {
+	return &Local{
+		Cache: cache.New(defaultTTL, cleanupInterval),
+	}
+}
+
+func (c *Local) Get(key string) ([]byte, error) {
+	if res, ok := c.Cache.Get(key); ok {
+		return res.([]byte), nil
+	}
+	return []byte{}, nil
+}
+
+func (c *Local) Set(key string, value []byte, ttl time.Duration) error {
+	c.Cache.Set(key, value, ttl)
+	return nil
+}
