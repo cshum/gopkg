456d77fb73d19b6617cb77a55f6d62c08ff44b20
Adrian Shum <adrian.shum@goxip.com>
2019-09-10 11:03:08 +0800

    errw.Validate errw.ValidateField

--- errw/error.go
+++ errw/error.go
@@ -6,27 +6,48 @@ import (
 )
 
 type Error struct {
-	Code    string `json:"code,omitempty"`
-	Message string `json:"message,omitempty"`
-	Status  int    `json:"status"`
+	Code    string                 `json:"code"`
+	Message string                 `json:"message"`
+	Status  int                    `json:"status"`
+	Extra   map[string]interface{} `json:"extra,omitempty"`
 }
 
 func (e *Error) Error() string {
 	return fmt.Sprintf("%s: %s", e.Code, e.Message)
 }
 
-func NotFound(message string) *Error {
+func NotFound(message string) error {
 	return &Error{
-		Status:  http.StatusNotFound,
 		Code:    "NotFoundError",
+		Status:  http.StatusNotFound,
 		Message: message,
 	}
 }
 
-func Unauthorized(message string) *Error {
+func Unauthorized(message string) error {
 	return &Error{
-		Status:  http.StatusUnauthorized,
 		Code:    "UnauthorizedError",
+		Status:  http.StatusUnauthorized,
 		Message: message,
+	}
+}
+
+func Validate(message string) error {
+	return &Error{
+		Code:    "ValidateError",
+		Status:  http.StatusBadRequest,
+		Message: message,
+	}
+}
+
+func ValidateField(field, reason string) error {
+	return &Error{
+		Code:    "ValidateError",
+		Status:  http.StatusBadRequest,
+		Message: fmt.Sprintf("%s %s", field, reason),
+		Extra: map[string]interface{}{
+			"field":  field,
+			"reason": reason,
+		},
 	}
 }
