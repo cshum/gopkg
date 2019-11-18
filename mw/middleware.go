2b0b94971a144c19dc53dc4c76521ba256a047fa
Adrian Shum <adrian.shum@goxip.com>
2019-11-18 17:11:16 +0800

    FileServer cleanup chi dependencies

--- mw/middleware.go
+++ mw/middleware.go
@@ -5,13 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"time"
-
-	"github.com/cshum/gopkg/rest"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 // RecoverHandler recovers from panic, passes error to handler
@@ -34,39 +27,17 @@ func RecoverHandler(
 	}
 }
 
-// AccessLogHandler zap logger middleware
-func AccessLogHandler(log func(string, ...zap.Field)) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			start := time.Now()
-			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
-			next.ServeHTTP(ww, r)
-			latency := time.Since(start)
-			fields := []zapcore.Field{
-				zap.Int("status", ww.Status()),
-				zap.Duration("took", latency),
-				zap.Int64("latency", latency.Nanoseconds()),
-				zap.String("remote", rest.RealIP(r)),
-				zap.String("request", r.RequestURI),
-				zap.String("method", r.Method),
-			}
-			log("access", fields...)
-		})
-	}
-}
-
 // FileServer conveniently sets up a http.FileServer handler to serve
 // static files from a http.FileSystem.
-func FileServer(r chi.Router, path, dirpath string) {
+func FileServer(path, dirpath string, get func(string, http.HandlerFunc)) {
 	if strings.ContainsAny(path, "{}*") {
 		panic(errors.New("FileServer does not permit URL parameters"))
 	}
 	fs := http.StripPrefix(path, http.FileServer(http.Dir(dirpath)))
 	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
 		path += "/"
 	}
 	path += "*"
-
-	r.Get(path, http.HandlerFunc(fs.ServeHTTP))
+	get(path, fs.ServeHTTP)
 }
