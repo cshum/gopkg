88d427f44bf48aae69e3323b51d6e482d56088ff
Adrian Shum <adrian.shum@goxip.com>
2019-11-18 11:46:31 +0800

    return not found err for hybrid cache

--- tinycache/cache_test.go
+++ tinycache/cache_test.go
@@ -7,21 +7,35 @@ import (
 )
 
 func DoTestCache(t *testing.T, c Cache) {
+	// not found
 	if v, err := c.Get("a"); v != nil || err != NotFound {
 		t.Error("should value nil and err not found")
 	}
 	if v, err := c.Get("a"); v != nil || err != NotFound {
 		t.Error("should value nil and err not found")
 	}
+	// set and found
 	if err := c.Set("a", []byte{'b'}, time.Minute*1); err != nil {
 		t.Error(err)
 	}
 	if v, err := c.Get("a"); string(v) != "b" || err != nil {
-		t.Error(err)
+		t.Error("should value and no error")
 	}
 	if v, err := c.Get("a"); string(v) != "b" || err != nil {
-		t.Error(err)
+		t.Error("should value and no error")
 	}
+	// set nil and found nil
+	/*
+		if err := c.Set("n", nil, time.Minute*1); err != nil {
+			t.Error("should nil value and no error")
+		}
+		if v, err := c.Get("n"); v != nil || err != nil {
+			t.Error("should nil value and no error")
+		}
+		if v, err := c.Get("n"); v != nil || err != nil {
+			t.Error("should nil value and no error")
+		}
+	*/
 }
 
 func TestLocal(t *testing.T) {
